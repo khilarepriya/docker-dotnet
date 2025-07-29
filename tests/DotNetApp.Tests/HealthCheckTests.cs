ac441958831f22125c930f0f4c8068ca27e362f9
khilarepriya <p.khilare@accenture.com>
2025-07-29 13:41:38 +0530

    Update test file

--- tests/DotNetApp.Tests/HealthCheckTests.cs
+++ tests/DotNetApp.Tests/HealthCheckTests.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Threading.Tasks;
 using Xunit;
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
@@ -8,22 +8,19 @@ public class HealthCheckTests
     [Fact]
     public async Task DotNetAppImage_ShouldRespondOnHealthEndpoint()
     {
-        string imageName = Environment.GetEnvironmentVariable("TEST_IMAGE_NAME") ?? "dotnetapp:latest";
+        var imageName = Environment.GetEnvironmentVariable("TEST_IMAGE_NAME") ?? "dotnetapp:latest";
 
         var container = new TestcontainersBuilder<TestcontainersContainer>()
             .WithImage(imageName)
-            .WithName("dotnetapp-test")
-            .WithPortBinding(6060, 80)
-            .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(80))
+            .WithPortBinding(6060, 6060)
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(r => r
+                .ForPort(6060)
+                .ForPath("/health")))
             .Build();
 
         await container.StartAsync();
-
-        var client = new HttpClient();
-        var response = await client.GetAsync("http://localhost:6060/health");
-        response.EnsureSuccessStatusCode();
-
-        await container.DisposeAsync();
+        Assert.True(container.State == TestcontainersStates.Running);
+        await container.StopAsync();
     }
 }
 
